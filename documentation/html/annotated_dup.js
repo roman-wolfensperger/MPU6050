66ceac865ffa61c1bb94e69963ba73bfc4dc3ef3
Roman <101247742+roman-wolfensperger@users.noreply.github.com>
2025-08-03 20:59:08 +0200

    Add html doxygen documentation files

---
+++ documentation/html/annotated_dup.js
@@ -0,0 +1,6 @@
+var annotated_dup =
+[
+    [ "mpu6050", "namespacempu6050.html", [
+      [ "MPU6050", "classmpu6050_1_1_m_p_u6050.html", "classmpu6050_1_1_m_p_u6050" ]
+    ] ]
+];
---
+++ documentation/html/namespaces_dup.js
@@ -0,0 +1,4 @@
+var namespaces_dup =
+[
+    [ "mpu6050", "namespacempu6050.html", "namespacempu6050" ]
+];
